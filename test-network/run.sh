074fd63effbc5ff36dca3fc16e620b6936522ddb
mkaifneo <kaif.basathiya@neosoftmail.com>
2023-04-10 23:22:52 +0530

    changed run.sh

--- test-network/run.sh
+++ test-network/run.sh
@@ -1,9 +1,16 @@
-rm -rf ../test-application/wallet
+# rm -rf ../test-application/wallet
 
-mongo hlf --eval "db.users.drop()"
+# mongo hlf --eval "db.users.drop()"
 
-./network.sh down
+# ./network.sh down
 
-./network.sh up createChannel -ca -s couchdb
+# ./network.sh up createChannel -ca -s couchdb
 
-./network.sh deployCC -ccn basic -ccp ../test-application/chaincode-javascript/ -ccl javascript
+# ./network.sh deployCC -ccn basic -ccp ../test-application/chaincode-javascript/ -ccl javascript
+
+cd ../test-application
+
+npm install 
+
+npm start
+
